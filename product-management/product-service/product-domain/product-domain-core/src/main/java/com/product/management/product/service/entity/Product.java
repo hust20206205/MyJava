38c829b7449721ba20bbdbe0494bcbe6578a6bef
Your New Name <nghiavu2k2abc@gmail.com>
2024-02-05 00:09:41 +0700

    Tạo DomainException và bắt đầu các phương thức initializeProduct,validateProduct

--- product-management/product-service/product-domain/product-domain-core/src/main/java/com/product/management/product/service/entity/Product.java
+++ product-management/product-service/product-domain/product-domain-core/src/main/java/com/product/management/product/service/entity/Product.java
@@ -2,14 +2,47 @@ package com.product.management.product.service.entity;
 
 import com.product.management.domain.entity.AggregateRoot;
 import com.product.management.domain.valueobject.ProductId;
+import com.product.management.product.service.exception.ProductDomainException;
 import com.product.management.product.service.valueobject.ProductStatus;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Product extends AggregateRoot<ProductId> {
    private final  String name;
     private   ProductStatus productStatus;
     private List<String> failureMessages;
+
+
+    
+    public void initializeProduct() {
+        setId(new ProductId(UUID.randomUUID()));
+        productStatus = ProductStatus.PENDING;
+    }
+
+
+
+    
+
+    public void validateProduct() {
+        if (getId() == null) {
+            throw new ProductDomainException("Product id cannot be null!");
+        }
+        if (productStatus == null) {
+            throw new ProductDomainException("Product status cannot be null!");
+        }
+        if (name == null) {
+            throw new ProductDomainException("Product name cannot be null!");
+        }
+    }
+
+
+//    create new
+//    update
+//
+//            delete
+
+
 
     private Product(Builder builder) {
    super.setId(builder.productId);
---
+++ product-management/product-service/product-domain/product-domain-core/src/main/java/com/product/management/product/service/exception/ProductDomainException.java
@@ -0,0 +1,14 @@
+package com.product.management.product.service.exception;
+
+import com.product.management.domain.exception.DomainException;
+
+public class ProductDomainException extends DomainException {
+
+    public ProductDomainException(String message) {
+        super(message);
+    }
+
+    public ProductDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
